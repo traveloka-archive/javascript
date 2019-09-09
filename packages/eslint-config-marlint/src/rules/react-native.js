7e9a6ce352e0b721d9926fc9efb12a0088deaf23
Budianto Tan <budianto.tan@gmail.com>
2019-09-09 22:19:11 +0700

    Add react-native/no-unused-styles

---
+++ packages/eslint-config-marlint/src/rules/react-native.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: ['react-native'],
+  rules: {
+    // https://github.com/Intellicode/eslint-plugin-react-native/blob/master/docs/rules/no-unused-styles.md
+    "react-native/no-unused-styles": "error",
+  },
+};
